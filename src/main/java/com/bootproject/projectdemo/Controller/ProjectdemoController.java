bcc9cf6200d3480cc89786b3044b22d39bda6505
Bagiyalakshana-C-B <bagiyalakshana@gmail.com>
2025-07-13 14:35:04 +0530

    Update1 ProjectdemoController.java

--- src/main/java/com/bootproject/projectdemo/Controller/ProjectdemoController.java
+++ src/main/java/com/bootproject/projectdemo/Controller/ProjectdemoController.java
@@ -2,17 +2,12 @@ package com.bootproject.projectdemo.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ProjectdemoController {
 	
 	@RequestMapping("/home")
 		public String indexbyjsp() {
-			return "pages/index";
+			return "index";
 		}
-		/*public String indexbyhtml() {
-			return "redirect:/index.html";
-		}
-	*/
 }
